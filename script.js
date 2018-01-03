a2d24d6fd123a0ccb970ec1c594084df107cbe53
sharifahmad <sharifahmad2061@gmail.com>
2018-01-03 15:02:23 +0500

    solves the card1 different problems and add some validations

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 $(function(){
     $('select').material_select();
     $('#form_sup').on('click',function(){
-        $('#card0').hide(1000);
-        $('#card1').show(1000);
+        $('#card0').hide(600);
+        $('#card1').show(600);
     });
     $('#form_sin').on('click',function(){
-        $('#card1').hide(1000);
-        $('#card0').show(1000);
+        $('#card1').hide(600);
+        $('#card0').show(600);
+    });
+
+    //sign in button
+    $('#button_sin').on('click',function(){
+
+
+        $('#form_sin').submit();
+    });
+
+    //sign up button
+    $('#button_sup').on('click',function(){
+        
+
+
+        $('#form_sup').submit();
     });
 });
